9aa2e33d65bfa57d4a05f81051c76ce980abf28b
hungcx <hungcx.work@gmail.com>
2025-05-10 10:12:46 +0000

    cicd: test update main.go

--- backend/cmd/api/main.go
+++ backend/cmd/api/main.go
@@ -10,7 +10,7 @@ func main() {
 	r := gin.Default()
 
 	r.GET("/hello", func(c *gin.Context) {
-		c.JSON(200, gin.H{"message": "Hello, World!"})
+		c.JSON(200, gin.H{"message": "Hello, World!2222"})
 	})
 
 	log.Println("Server running at :8080")

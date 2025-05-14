f3513afeae0978f04db13772a4e51d394815cd9b
Xuân Hưng Cù <aristo.hung@htcglobal.com.vn>
2025-05-14 03:52:23 +0000

    Devops/init project

--- backend/cmd/api/main.go
+++ backend/cmd/api/main.go
@@ -10,11 +10,14 @@ func main() {
 	r := gin.Default()
 
 	r.GET("/hello", func(c *gin.Context) {
-		c.JSON(200, gin.H{"message": "Hello, World!"})
+		c.JSON(200, gin.H{"message": "Hello, World!2222"})
 	})
 
 	log.Println("Server running at :8080")
 
-	r.Run(":8080")
+	err := r.Run(":8080");
+	if err != nil {
+		log.Fatalf("failed to run server: %v", err)
+	}
 
 }

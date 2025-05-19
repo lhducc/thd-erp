45e9a734eeb2ba5a451406ffb298ce8855704add
Xuân Hưng Cù <aristo.hung@htcglobal.com.vn>
2025-05-19 14:16:48 +0000

    feat: Update ButtonCreateProfile.tsx

--- frontend/src/components/ButtonCreateProfile.tsx
+++ frontend/src/components/ButtonCreateProfile.tsx
@@ -38,38 +38,3 @@ const ButtonCreateProfile = () => {
 };
 
 export default ButtonCreateProfile;
-
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
-import { Button } from "./ui/button";
-import { Plus } from "lucide-react";
-import { useState } from "react";
-
-const ButtonCreateProfile = () => {
-  const [openIntern, setOpenIntern] = useState(false);
-  const [openEmployee, setOpenEmployee] = useState(false);
-  // const [openIntern, setOpenIntern] = useState(false);
-  return (
-    <>
-      <DropdownMenu>
-        <DropdownMenuTrigger>
-          <Button>
-            <Plus />
-            Thêm nhân viên
-          </Button>
-        </DropdownMenuTrigger>
-        <DropdownMenuContent>
-          <DropdownMenuItem>Nhân viên chính thức</DropdownMenuItem>
-          <DropdownMenuItem>Thực tập sinh</DropdownMenuItem>
-          <DropdownMenuItem>Công tác viên</DropdownMenuItem>
-        </DropdownMenuContent>
-      </DropdownMenu>
-    </>
-  );
-};
-
-export default ButtonCreateProfile;

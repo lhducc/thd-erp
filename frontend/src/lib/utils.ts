a63701acd8f26032c0faf1416a1cc907ec99a92a
lhducc <pduc2200@gmail.com>
2025-10-02 13:35:07 +0700

    chore: add logo THD and fix time history

--- frontend/src/lib/utils.ts
+++ frontend/src/lib/utils.ts
@@ -21,8 +21,8 @@ export function formatDate(isoDate: string): string {
 
 export const getTimeFromTimestamp = (timestamp: string): string => {
   const date = new Date(timestamp);
-  const hours = String(date.getUTCHours()).padStart(2, '0');
-  const minutes = String(date.getUTCMinutes()).padStart(2, '0');
+  const hours = String(date.getHours()).padStart(2, '0');
+  const minutes = String(date.getMinutes()).padStart(2, '0');
   return `${hours}:${minutes}`;
 };
 
